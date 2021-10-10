79632b44339bc54f3fd10aa0347ed3a9b1e632ea
charlieroberts <charlie@charlie-roberts.com>
2021-10-10 02:59:27 -0400

    pattern filter management

--- js/seq.js
+++ js/seq.js
@@ -15,7 +15,7 @@ module.exports = function( Gibber ) {
     }
 
     // trigger autotrig patterns
-    if( key === 'note' || key === 'chord' || key === 'trigger' ) {
+    if( key === 'note' || key === 'chord' || key === 'trigger' || key === 'notef' || key === 'pickplay' ) {
       values.addFilter( ( args,ptrn ) => {
         if( ptrn.seq.target.autotrig !== undefined ) {
           for( let s of ptrn.seq.target.autotrig ) {
@@ -25,6 +25,43 @@ module.exports = function( Gibber ) {
         return args
       })
     } 
+  }
+
+  const addTimingFilters = function( seq,key,renderMode ) {
+    const __timings = seq.timings
+    if( __timings.randomFlag ) {
+      __timings.addFilter( ( args,ptrn ) => {
+        const range = ptrn.values.length - 1
+        const idx = Math.round( Math.random() * range )
+        return [ ptrn.values[ idx ], 1, idx ] 
+      })
+      //for( let i = 0; i < this.values.randomArgs.length; i+=2 ) {
+      //  valuesPattern.repeat( this.values.randomArgs[ i ], this.values.randomArgs[ i + 1 ] )
+      //}
+    }
+
+    const filter = renderMode === 'Audio' 
+      ? (args,ptrn) => {
+        if( typeof args[0] === 'number' ) {
+          args[0] = Gibberish.Clock.time( args[0] )
+        }
+        return args
+      }
+      : (args,ptrn) => {
+        if( typeof args[0] === 'number' ) {
+          args[0] = Gibber.Clock.time( args[0] )
+        }
+        return args
+      }  
+
+    seq.timings.addFilter( (args,ptrn)=>{
+      if( args[0] === 0 ) {
+        ptrn.seq.stop()
+        console.warn( 'sequencer attempting to fire with a time of zero; this will result in an infinite loop, so the sequencer has been stopped.' )
+      }
+      return args
+    })
+    seq.timings.addFilter( filter ) 
   }
 
   const Seq = function( props ) { 
@@ -45,79 +82,69 @@ module.exports = function( Gibber ) {
       __values = __values.render()
     }
 
+    if( Array.isArray( __values ) && __values.length <= 0 ) throw Error('arrays passed to sequences must have at least one value inside of them')
     // convert to pattern if needed and render
     const values = Array.isArray( __values ) 
       ? Gibber.Pattern( ...__values ).render()
       : typeof __values === 'function' && __values.isPattern 
         ? __values.render()
-        : Gibber.Pattern( __values ).render()
+        : __values.requiresRender 
+          ? __values
+          : Gibber.Pattern( __values ).render()
 
     // if an array of values is passed, let users call pattern method on that array, for example:
     // a.note.seq( b=[0,1,2,3], 1/4 )
     // b.transpose.seq( 1,1 )
     if( Array.isArray( __values ) ) {
       Object.assign( __values, values )
-    } else if (typeof __values === 'object' && __values.type==='gen') {
+      __values.addFilter = values.addFilter.bind( values )
+      __values.removeFilter = values.removeFilter.bind( values )
+      __values.inspect = values.inspect.bind( values )
+    } else if( typeof __values === 'object' && __values.type==='gen' ) {
       props.values.addFilter = values.addFilter.bind( values )
+      props.values.removeFilter = values.removeFilter.bind( values )
+      props.values.inspect = values.inspect.bind( values )
     }
 
     // process time values
-    if( Gibber[ render ].timeProps[ target.name ] !== undefined && Gibber[ render ].timeProps[ target.name ].indexOf( key ) !== -1  ) {
-      const filter = render === 'Audio' 
-        ? (args,ptrn) => {
-            args[0] = Gibberish.Clock.time( args[0] )
-            return args
-          }
-        : (args,ptrn) => {
-            args[0] = Gibber.Audio.Clock.time( args[0] )
-            return args
-          }
+    if( target !== undefined ) {
+      if( Gibber[ render ].timeProps[ target.name ] !== undefined 
+        && Gibber[ render ].timeProps[ target.name ].indexOf( key ) !== -1  ) {
 
-      values.addFilter( filter )
-    }
+        const filter = render === 'Audio' 
+          ? (args,ptrn) => {
+              args[0] = Gibberish.Clock.time( args[0] )
+              return args
+            }
+          : (args,ptrn) => {
+              args[0] = Gibber.Audio.Clock.time( args[0] )
+              return args
+            }
 
-    let timings
-    if( Array.isArray( __timings ) ) {
-      timings  = Gibber.Pattern( ...__timings )
-    }else if( typeof __timings === 'function' && __timings.isPattern === true ) {
-      timings = __timings
-    }else if( __timings !== undefined && __timings !== null ) {
-      timings = Gibber.Pattern( __timings )
-    }else{
-      timings = null
-      autotrig = true
-    }
-
-    if( timings !== null ) timings = timings.render()
-
-    if( autotrig === false ) {
-      if( __timings.randomFlag ) {
-        timings.addFilter( ( args,ptrn ) => {
-          const range = ptrn.values.length - 1
-          const idx = Math.round( Math.random() * range )
-          return [ ptrn.values[ idx ], 1, idx ] 
-        })
-        //for( let i = 0; i < this.values.randomArgs.length; i+=2 ) {
-        //  valuesPattern.repeat( this.values.randomArgs[ i ], this.values.randomArgs[ i + 1 ] )
-        //}
+        values.addFilter( filter )
       }
+    }
+ 
+    const timings = Array.isArray( __timings ) 
+      ? Gibber.Pattern( ...__timings ).render()
+      : typeof __timings === 'function' && __timings.isPattern 
+        ? __timings.render()
+        : __timings === undefined || __timings === null 
+          ? null
+          : __timings.requiresRender
+            ? __timings
+            : Gibber.Pattern( __timings ).render()
+
+
+    if( timings === null ) autotrig = true
+
+    if( Array.isArray( __timings ) ) {
+      Object.assign( __timings, timings )
+      __timings.addFilter = timings.addFilter.bind( timings )
+    }
+    if( autotrig === false ) {
       timings.output = { time:'time', shouldExecute:0 }
       timings.density = 1
-      const filter = render === 'Audio' 
-        ? (args,ptrn) => {
-          if( typeof args[0] === 'number' )
-            args[0] = Gibberish.Clock.time( args[0] )
-
-          return args
-        }
-        : (args,ptrn) => {
-          if( typeof args[0] === 'number' )
-            args[0] = Gibber.Clock.time( args[0] )
-
-          return args
-        }  
-
-      timings.addFilter( filter ) 
 
       // XXX delay annotations so that they occur after values annotations have occurred. There might
       // need to be more checks for this flag in the various annotation update files... right now
@@ -126,7 +153,7 @@ module.exports = function( Gibber ) {
     }
 
     // if an array is passed to the seq, enable users to call pattern methods on array
-    if( Array.isArray( __timings ) ) Object.assign( __timings, timings )
+    //if( Array.isArray( __timings ) ) Object.assign( __timings, timings )
 
     const clear = render === 'Audio'
       ? function() {
@@ -169,33 +196,38 @@ module.exports = function( Gibber ) {
     // XXX need to abstract this so that a graphics sequencer could also be called...
     const seq = Gibber.Audio.Gibberish.Sequencer({ values, timings, density, target, key, priority, rate:1/*Gibber.Clock.AudioClock*/, clear, autotrig, mainthreadonly:props.mainthreadonly })
 
-    values.setSeq( seq )
+    if( values.setSeq ) values.setSeq( seq )
 
     addValuesFilters( seq,key )
 
     if( autotrig === false ) {
-      timings.setSeq( seq )
+      addTimingFilters( seq,key,render )
+      if( timings.setSeq ) timings.setSeq( seq )
     }else{
-      if( target.autotrig === undefined ) {
-        target.autotrig = []
-        Gibber.Audio.Gibberish.worklet.port.postMessage({
-          address:'property',
-          name:'autotrig',
-          value:[],
-          object:target.id
-        })
+      if( target !== undefined ) {
+        if( target.autotrig === undefined ) {
+          target.autotrig = []
+          Gibber.Audio.Gibberish.worklet.port.postMessage({
+            address:'property',
+            name:'autotrig',
+            value:[],
+            object:target.id
+          })
 
-      }
-      // object name key value
-      if( Gibber.Audio.Gibberish.mode === 'worklet' ) {
-        Gibber.Audio.Gibberish.worklet.port.postMessage({
-          address:'addObjectToProperty',
-          name:'autotrig',
-          object:target.id,
-          key:target.autotrig.length,
-          value:seq.id
-        })
-        target.autotrig.push( seq )
+        }
+        // object name key value
+        if( Gibber.Audio.Gibberish.mode === 'worklet' ) {
+          Gibber.Audio.Gibberish.worklet.port.postMessage({
+            address:'addObjectToProperty',
+            name:'autotrig',
+            object:target.id,
+            key:target.autotrig.length,
+            value:seq.id
+          })
+          target.autotrig.push( seq )
+        }
+      }else{
+        throw Error('you must define timings for any sequence without a target')
       }
     } 
 
@@ -218,12 +250,19 @@ module.exports = function( Gibber ) {
         prevSeq.clear();
       }
 
+
       // XXX you have to add a method that does all this shit on the worklet. crap.
       targetProp.sequencers[ props.number ] = seq
       targetProp[ props.number ] = seq 
       //target.__sequencers.push( seq )
-      seq.start( Gibber.Audio.Clock.time( delay ) )
+      if( typeof delay !== 'function' ) { 
+        seq.start( Gibber.Audio.Clock.time( delay ) )
+      } else {
+        delay.seqs.push( seq )
+      }
     }
+
+    Gibber.publish( 'new sequence', seq )
 
     return seq
   }
